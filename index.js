8fcd713109d6001d1d34ff1cd1027bc04ff4c4f6
David Rodriguez <alejandro20rodriguez04@gmail.com>
2020-07-06 21:28:49 -0700

    Authors API

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const { config } = require('./config/');
-const books = require('./routes/books');
+const express = require("express");
+const { config } = require("./config/");
+const books = require("./routes/books");
+const authors = require("./routes/authors");
 const app = express();
 
 //It can undestand json
 app.use(express.json());
-//Uses routes of books
+//
+//Authors
+authors(app);
+//Books
 books(app);
 
 app.listen(config.port, (err) => {
-   if (err) console.log(err);
-   else console.log(`Listening http://localhost:${config.port}`);
+  if (err) console.log(err);
+  else console.log(`Listening http://localhost:${config.port}`);
 });
